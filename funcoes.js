16456a455ebd4d869d94e57d6592076380dd93df
Leonardo <leobrustolin1@gmail.com>
2024-12-02 18:00:58 -0300

    funcoes

---
+++ funcoes.js
@@ -0,0 +1,7 @@
+function exibeInfosEstudande(nome, nota) {
+    return `O nome é ${nome} e a nota é ${nota}`;   
+}
+
+console.log(exibeInfosEstudande('Leonardo', 10));
+console.log(exibeInfosEstudande('Ana', 20));
+
--- operadores-logicos.js
+++ operadores-logicos.js
@@ -1,5 +1,5 @@
 const notaFinal = 6;
-const faltas = 2;
+const faltas = 4;
 const advertencias = 0;
 
 if (notaFinal < 7 && faltas > 4) {
@@ -8,7 +8,7 @@ if (notaFinal < 7 && faltas > 4) {
   console.log('não foi reprovado por falta');
 }
 
-if (faltas >= 2 && !advertencias) {
+if (faltas <= 4 && !advertencias) {
   console.log('recebeu bônus');
 } else {
   console.log('não recebeu bônus');
